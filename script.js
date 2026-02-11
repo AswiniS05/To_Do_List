103e544de2389fc518a0acb8b26f7fa90ac75540
AswiniS05 <134632105+AswiniS05@users.noreply.github.com>
2026-02-11 16:14:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+let currentFilter = "all";
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function renderTasks() {
+    const list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+
+        if (currentFilter === "active" && task.completed) return;
+        if (currentFilter === "completed" && !task.completed) return;
+
+        const li = document.createElement("li");
+        li.className = task.completed ? "completed" : "";
+
+        li.innerHTML = `
+            <span onclick="toggleTask(${index})">${task.text}</span>
+            <button onclick="deleteTask(${index})">X</button>
+        `;
+
+        list.appendChild(li);
+    });
+}
+
+function addTask() {
+    const input = document.getElementById("taskInput");
+
+    if (input.value.trim() === "") return;
+
+    tasks.push({
+        text: input.value,
+        completed: false
+    });
+
+    input.value = "";
+    saveTasks();
+    renderTasks();
+}
+
+function toggleTask(index) {
+    tasks[index].completed = !tasks[index].completed;
+    saveTasks();
+    renderTasks();
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    saveTasks();
+    renderTasks();
+}
+
+function filterTasks(filter) {
+    currentFilter = filter;
+    renderTasks();
+}
+
+renderTasks();
